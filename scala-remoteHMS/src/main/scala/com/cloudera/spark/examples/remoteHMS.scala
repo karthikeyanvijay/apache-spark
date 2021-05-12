bfedcb5c930660b78c3f752cd3c9e63f3f75ee65
Vijay Anand Karthikeyan <vijayanand.karthikeyan@gmail.com>
2021-05-12 08:24:56 -0500

    Added code for Remote HMS access

---
+++ scala-remoteHMS/src/main/scala/com/cloudera/spark/examples/remoteHMS.scala
@@ -0,0 +1,30 @@
+package com.cloudera.spark.examples
+import org.apache.spark.sql.SparkSession
+
+/**
+ * @author Vijay Anand Karthikeyan
+ */
+object remoteHMS {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  final val sparkApp = "remoteHMS"
+
+  def main(args : Array[String]) {
+    //println("concat arguments = " + foo(args))
+    val spark = SparkSession.builder
+      .master("local[*]")
+      .appName(sparkApp)
+      .config("spark.sql.warehouse.dir", "/tmp/")
+      .config("javax.jdo.option.ConnectionURL", "jdbc:mysql://nightly71x-1.nightly71x.root.hwx.site:3306/hive1")
+      .config("javax.jdo.option.ConnectionUserName", "hive1")
+      .config("javax.jdo.option.ConnectionPassword", "hive1")
+      .config("javax.jdo.option.ConnectionDriverName", "com.mysql.jdbc.Driver")
+      .config("spark.driver.bindAddress", "127.0.0.1")
+      //.config("spark.hive.exec.scratchdir", "/tmp/hive")
+      .enableHiveSupport()
+      .getOrCreate()
+     spark.sql("show databases").show()
+
+  }
+
+}
